ef47dc254bf3069c406b8d69b5905546f1034f16
Jerry Pham <chalizardmisadon@yahoo.com>
2019-06-08 17:11:50 -0400

    add localization search algorithm; improve search algorithm to skip directories

--- autogen_clang_complete.py
+++ autogen_clang_complete.py
@@ -15,26 +15,74 @@ dirIgnore = [
 	"/."
 ]
 
-def addPredefinedPath():
-	root = "."
-	fd = open(".clang_complete", "w")
-	for (directory, dirname, filename) in os.walk(root):
-		if not any(ignore in directory for ignore in dirIgnore):
-			fd.write("-I%s\n" % directory)
-			# print(directory)
-	fd.close()
+
+def addPreOrderSearch_v1(fd):
+	print("File is generated using Pre Order Searching v1. Loop through all directories.")
+	root = "./"
+	for (dirpath, subdirs, files) in os.walk(root):
+		if not any(ignore in dirpath for ignore in dirIgnore):
+			fd.write("-I%s\n" % dirpath)
+
+
+def addPreOrderSearch_v2(fd):
+	print("File is generated using Pre Order Searching v2. Skip all ignored directories")
+	root = "./"
+	for (dirpath, subdirs, files) in os.walk(root):
+		subdirs[:] = [d for d in subdirs if d not in dirIgnore]
+		fd.write("-I%s\n" % dirpath)
+
+
+def addFileToRootSearch(fd, currDir, childDir=''):
+	if (currDir == ''):
+		print("Root Folder reached in recursive search.")
+		return
+
+	for (dirpath, subdirs, files) in os.walk(currDir, topdown=True):
+		if (dirpath in currDir):
+			subdirs[:] = [d for d in subdirs if d not in childDir]
+		subdirs[:] = [d for d in subdirs if d not in dirIgnore]
+		fd.write("-I%s\n" % dirpath)
+
+	addFileToRootSearch(fd, os.path.dirname(currDir), os.path.basename(currDir))
+
 
 def main():
-	addPredefinedPath()
-	# print("Generated .clang_complete")
+	fd = open(".clang_complete", "w")
+	addPreOrderSearch_v2(fd)
+	fd.close()
+	printFinishGen()
+
+
+def printFinishGen():
+	print("Generated .clang_complete @ \"%s\"" % os.getcwd())
+
 
 try:
-	class ClangCompleteGenCommand(sublime_plugin.WindowCommand):
+	class GenClangCompleteCommand(sublime_plugin.WindowCommand):
 		def run(self):
-			dirVar = self.window.folders()[0]
-			os.chdir(dirVar)
-			main()
-			print("Generated .clang_complete @ %s" % dirVar)
+			self.addFileToRootSearch()
+
+		def addFileToRootSearch(self):
+			projVar = self.window.extract_variables()
+			if projVar["folder"] not in projVar["file_path"]:
+				print("\nCurrent opened Folder \"%s\"\ndoes not contain File \"%s\"\n" % (projVar["folder"], projVar["file_path"]))
+				return
+			
+			os.chdir(projVar["folder"])
+			fd = open(".clang_complete", "w")
+			addFileToRootSearch(fd, "./" + os.path.relpath(projVar["file_path"], projVar["folder"]))
+			fd.close()
+			printFinishGen()
+
+		def addPreOrderSearch(self):
+			projVar = self.window.extract_variables()
+			os.chdir(projVar["folder"])
+			fd = open(".clang_complete", "w")
+			addPreOrderSearch_v2(fd)
+			fd.close()
+			printFinishGen()
+
+
 except Exception:
 	print("Script is not running in a Sublime environment!\nGenerated .clang_complete in current script directory.")
 	main()
